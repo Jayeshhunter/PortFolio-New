554242950cd3dd5e9fa81c1b40adfa471226d541
Jayesh Jayanandan <67648301+Jayeshhunter@users.noreply.github.com>
2022-06-24 10:27:16 +0530

    feat: responsive hero

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -63,7 +63,7 @@ function Hero() {
         </section>
       ) : (
         <section className="text-gray-600 dark:text-gray-100 body-font">
-          <div className="container mx-auto flex px-20 py-24 md:flex-row flex-col items-center">
+          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
               <img
                 className="object-cover mx-auto object-center rounded"
