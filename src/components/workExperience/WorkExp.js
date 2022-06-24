b2542b769f20f1132c10e52c7ee70ca07ac2683f
Jayesh Jayanandan <67648301+Jayeshhunter@users.noreply.github.com>
2022-06-24 10:36:27 +0530

    feat: responsive changes

--- src/components/workExperience/WorkExp.js
+++ src/components/workExperience/WorkExp.js
@@ -13,7 +13,7 @@ function WorkExp() {
   return (
     <section className="text-gray-600 body-font" id="project">
       {width > 500 ? (
-        <div className="container px-24 py-24 mx-auto">
+        <div className="container px-10 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
             <div className="flex flex-col text-center w-full mb-20">
               <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 dark:text-gray-300">
